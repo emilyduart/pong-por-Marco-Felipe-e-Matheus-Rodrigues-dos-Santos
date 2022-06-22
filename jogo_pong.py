556d4798617921fc4ccf1d8daf694f3427a0e33e
Marco Túlio <marcotuliodvaz@gmail.com>
2022-06-22 19:03:16 -0300

    movimento dos jogadores

--- jogo_pong.py
+++ jogo_pong.py
@@ -13,8 +13,11 @@ preto = (0, 0, 0)
 
 larguraJogador , alturaJogador = 20, 100
 
+
+
 class jogadores:
     cor = branco
+    velocidade = 4
 
     def __init__(self, x, y , larguraJogador, alturaJogador):
         self.x = x
@@ -25,6 +28,12 @@ class jogadores:
     def jogo(self , janelaJogo):
         pygame.draw.rect(janelaJogo, self.cor, (self.x, self.y, self.larguraJogador, self.alturaJogador))
 
+    def movimento(self, up=True):
+        if up:
+            self.y -= self.velocidade
+        else:
+            self.y += self.velocidade
+
 def jogo(janelaJogo, players):
     janelaJogo.fill(preto)
 
@@ -33,13 +42,23 @@ def jogo(janelaJogo, players):
 
     pygame.display.update()
 
+def movimentoDosJogadores(keys, jogadorUm, jogadorDois):
+    if keys[pygame.K_w] and jogadorUm - jogadorUm >= 0:
+        jogadorUm.movimento(up=True)
+    if keys[pygame.K_s] and jogadorUm + jogadorUm <= altura:
+        jogadorUm.movimento(up=False)
+    if keys[pygame.K_UP] and jogadorDois - jogadorDois >= 0:
+        jogadorDois.movimento(up=True)
+    if keys[pygame.K_DOWN] and jogadorDois + jogadorDois <= altura:
+        jogadorDois.movimento(up=False)
+
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
-    jogadorUm = jogadores(10, altura/2 - alturaJogador/2, 20, 100)
-    jogadorDois = jogadores(largura - 30, altura/2 - alturaJogador/2, 20, 100)
+    jogadorUm = jogadores(10, altura//2 - alturaJogador//2, larguraJogador , alturaJogador )
+    jogadorDois = jogadores(largura - 30, altura//2 - alturaJogador//2,larguraJogador , alturaJogador )
    
 
     while run:
@@ -49,6 +68,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+        movimentoDosJogadores(keys, jogadorUm, jogadorDois)
     pygame.quit()
 
 if __name__ == '__main__':
